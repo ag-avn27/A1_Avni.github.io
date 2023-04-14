b0eb9b2209ba108c4373be197d09af895bf85ad7
ag-avn27 <130656758+ag-avn27@users.noreply.github.com>
2023-04-14 10:21:48 +0530

    Add files via upload

---
+++ Prompt.js
@@ -0,0 +1,7 @@
+
+let name = prompt("What is your name?");
+console.log(name)
+let age = prompt("How old are you?");
+console.log(age)
+let hobby = prompt("What is your favorite hobby?");
+console.log(hobby)
